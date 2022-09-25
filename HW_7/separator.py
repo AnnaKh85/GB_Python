b4916918737e0baec95ea8b757f222e7a61e0489
Anna <Anna.khizhnyakova@gmail.com>
2022-09-25 17:34:11 +0300

    просто функции - описания разделителей

---
+++ HW_7/separator.py
@@ -0,0 +1,20 @@
+path_to_file = 'log.csv'
+
+def whitespase():
+    with open(path_to_file) as file:
+        for num_line, line in enumerate(file):
+            num_line = 1 + num_line
+            with open('separator_whitespace.csv','a') as file:
+                if num_line % 2 == 0:
+                    file.write(line)
+                elif num_line > 1:
+                    file.write('\n')
+
+def comma():
+    with open(path_to_file) as file:
+        for num_line, line in enumerate(file):
+            num_line = 1 + num_line
+            with open('separator_comma.csv','a') as file:
+                if num_line %2 == 0 and num_line!=2:
+                    a = line.replace('\n',',').replace('|','').replace('         ',' ')
+                    file.write(a)

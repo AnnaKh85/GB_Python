b519978feead03065e3b7ca309f66176fa5f0405
Anna <Anna.khizhnyakova@gmail.com>
2022-09-25 17:30:04 +0300

    Описание полей данных

---
+++ HW_7/data.py
@@ -0,0 +1,7 @@
+def get_data():
+    surname = input('Surname: ')
+    name = input('Name: ')
+    number = input('Number_Phone: ')
+    description = input('Discription: ')
+    list = [surname, name, number, description]
+    return list

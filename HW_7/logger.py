ee2695051f743d9a694344c7ccc326b812c4e900
Anna <Anna.khizhnyakova@gmail.com>
2022-09-25 17:33:19 +0300

    запись лога

---
+++ HW_7/logger.py
@@ -0,0 +1,7 @@
+from tabulate import tabulate
+
+head = ["Surname", "Name", "Phone number", "Discription"]
+
+def add_data(d):
+    with open ('log.csv', 'a') as file:
+        file.write(tabulate(d, headers=head, tablefmt="grid"))
